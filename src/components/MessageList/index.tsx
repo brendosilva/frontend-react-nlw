21d9ba4b7374af3e46dad4e1a087b3b101707bf5
Brendo Silva <breendo.01@gmail.com>
2021-10-21 13:55:55 -0300

    intregacao com api backend

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,6 +1,48 @@
+import { api } from "./../../services/api"
+
 import styles from "./styles.module.scss"
+import logoImage from "../../assets/logo.svg"
+import { useEffect, useState } from "react"
+
+type Message = {
+  id: string;
+  text: string;
+  user: {
+    name: string;
+    avatar_url: string;
+    login: string;
+  }
+
+}
+
 export function MessageList() {
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    api.get<Message[]>('messages/lastTree').then(response => {
+      setMessages(response.data);
+    });
+  }, []);
+
   return (
-    <h1>MessageList</h1>
+    <div className={styles.messageListWrapper}>
+      <img src={logoImage} alt="DoWhile 2021" />
+      <ul className={styles.messageList}>
+        {messages.map(message => {
+          return (
+            <li key={message.id} className={styles.message}>
+              <p className={styles.messageContent}>{message.text}</p>
+              <div className={styles.messageUser}>
+                <div className={styles.userImage}>              
+                  <img src={message.user.avatar_url} alt={message.user.login} />
+                </div>
+                <span>{message.user.name}</span>
+              </div>
+            </li>
+          );
+        })}
+        
+      </ul>
+    </div>
   )
 }
